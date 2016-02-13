1ab89821cf65623344f03550870d094a570391a6
Raul Glogovetan <raul@petabyte.ro>
2016-02-13 15:17:02 +0200

    Elements generator

--- sources/scripts/boilerplate.js
+++ sources/scripts/boilerplate.js
@@ -3,3 +3,19 @@ const Hooks = {
 	preloader: $('#preloader'),
 	error: $('#ajax-error')
 }
+const Proto = {
+	generateHeading: (title) => {
+		return (
+			'<h3>' + title + '</h3>'
+		);
+	},
+	generateStreamer: (link, name, image, status) => {
+		return (
+			'<div class="channel"><a href="' + link + '">' +
+			'<span class="image" style="background-url(\"' + image + '\")"></span>' +
+			'<p class="title">' + name + '</p>' +
+			'<p class="status">' + status + '</p>' +
+			'</a></div>'
+		);
+	}
+};
